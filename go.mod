3f09d1885da5e3bb16a37300905f6196d17c94c2
. Facu . <facu.tk@gmail.com>
2025-11-01 13:13:29 -0300

    change .env loading

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/facutk/tablechat
 
 go 1.25.2
 
-require github.com/a-h/templ v0.3.943 // indirect
+require github.com/go-chi/chi/v5 v5.2.3
+
+require (
+	github.com/a-h/templ v0.3.943 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -6,20 +6,29 @@ import (
 	"os"
 
 	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	_ = godotenv.Load()
+
 	gitsha, ok := os.LookupEnv("GITSHA")
 	if !ok || gitsha == "" {
 		gitsha = "unknown"
 	}
 	component := hello(fmt.Sprintf("tablechat.me (%s)", gitsha))
-	http.Handle("/", templ.Handler(component))
+
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r2 *http.Request) {
+		templ.Handler(component).ServeHTTP(w, r2)
+	})
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	// server static files
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", fs)
+	// fs := http.FileServer(http.Dir("./static"))
+	// http.Handle("/static/", fs)
 
 	fmt.Println("Listening on :3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
